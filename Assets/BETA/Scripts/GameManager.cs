d210bc1e3fe00792db2cb31893590e172e1d575d
최성은 <ondor1224@gmail.com>
2026-08-26 21:02:55 +0900

    asdas
    sdasd

---
+++ Assets/BETA/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BETA7
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        public GameObject[] Enemys;
+        public Vector3 spawnValue;
+        public int enemyCount;
+
+        public float spawnWait;
+        public float startWait;
+        
+
+        public List<GameObject> listEnemys = new List<GameObject>();
+
+        public enum GameStatus
+        {
+            none,
+            play,
+            gameOber,
+            gameClear
+        }
+
+        public GameStatus gameStatus = GameStatus.none;
+
+        void Start()
+        {
+            gameStatus = GameStatus.play;
+            StartCoroutine(SpawnEnemy());
+        }
+
+        void Update()
+        {
+            
+        }
+
+        IEnumerator SpawnEnemy()
+        {
+            yield return new WaitForSeconds(spawnWait);
+            while (true)
+            {
+                for(int i=0; i < enemyCount; i++)
+                {
+                    GameObject enemy = Enemys[Random.Range(0, Enemys.Length)];
+                    Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                    Quaternion spawnRotation = Quaternion.identity;
+                    listEnemys.Add(Instantiate(enemy, spawnPosition, spawnRotation));
+                    yield return new WaitForSeconds(spawnWait);
+                }
+            }
+            
+        }
+
+
+
+
+    }
+}
